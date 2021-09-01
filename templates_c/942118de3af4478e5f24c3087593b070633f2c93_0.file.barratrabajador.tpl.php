df4a4baf54a9ef7763e1c7fa1fdf2d6be697f31e
Maria13579 <patymsr3@gmail.com>
2021-09-01 12:10:30 -0600

    a

--- templates_c/942118de3af4478e5f24c3087593b070633f2c93_0.file.barratrabajador.tpl.php
+++ templates_c/942118de3af4478e5f24c3087593b070633f2c93_0.file.barratrabajador.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-08-27 17:21:56
+/* Smarty version 3.1.39, created on 2021-09-01 20:08:51
   from 'C:\adobeTemp\htdocs\AtencionCliente\templates\Navs\barratrabajador.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61290314801f05_38742581',
+  'unifunc' => 'content_612fc1b3a442a9_28213192',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '942118de3af4478e5f24c3087593b070633f2c93' => 
     array (
       0 => 'C:\\adobeTemp\\htdocs\\AtencionCliente\\templates\\Navs\\barratrabajador.tpl',
-      1 => 1630075165,
+      1 => 1630205834,
       2 => 'file',
     ),
   ),
@@ -20,11 +20,11 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61290314801f05_38742581 (Smarty_Internal_Template $_smarty_tpl) {
+function content_612fc1b3a442a9_28213192 (Smarty_Internal_Template $_smarty_tpl) {
 ?><br/> <br/>
 <div class="row">
    <div class="col s2">
-      <nav class="center-align purple lighten-4">OPCIONES</nav>
+      <nav class="center-align amber lighten-1">OPCIONES</nav>
          <div class="collection">
          <a href="?class=Direcciones&method=Visproblematecnico" class="collection-item ">Problemas tecnicos</a>
          <a href="?class=Direcciones&method=Visresoluciondudas" class="collection-item ">Resolucion de dudas</a>
--- templates_c/b333ec23b3eef7553a95080e8a72bd9bed7b6496_0.file.ProTec.tpl.php
+++ templates_c/b333ec23b3eef7553a95080e8a72bd9bed7b6496_0.file.ProTec.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-08-29 04:48:50
+/* Smarty version 3.1.39, created on 2021-09-01 20:08:58
   from 'C:\adobeTemp\htdocs\AtencionCliente\templates\Clientes\ProTec.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_612af592d91431_94681463',
+  'unifunc' => 'content_612fc1ba176065_65113079',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b333ec23b3eef7553a95080e8a72bd9bed7b6496' => 
     array (
       0 => 'C:\\adobeTemp\\htdocs\\AtencionCliente\\templates\\Clientes\\ProTec.tpl',
-      1 => 1630205323,
+      1 => 1630344710,
       2 => 'file',
     ),
   ),
@@ -24,7 +24,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/Cabeceras/Footer.tpl' => 1,
   ),
 ),false)) {
-function content_612af592d91431_94681463 (Smarty_Internal_Template $_smarty_tpl) {
+function content_612fc1ba176065_65113079 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:templates/Cabeceras/Header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:Navs/Encabezado.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:Navs/baracliente.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
@@ -34,7 +34,7 @@ $_smarty_tpl->_subTemplateRender("file:Navs/baracliente.tpl", $_smarty_tpl->cach
 <div class="center-align"><h1 >Problemas tecnicos</h1> </div>
 <br/><br/>
 <div >
-<div class="center-align"><a href="?class=Direcciones&method=Codigo" class="waves-effect waves-light btn"><i class="material-icons right">
+<div class="center-align"><a href="?class=codigo&method=Codigoprotec" class="waves-effect waves-light btn"><i class="material-icons right">
 </i>Generar Codigo</a></div>
 
 </div>
