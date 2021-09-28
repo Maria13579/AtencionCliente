c3aa97e0b3ec4cc5be25b74054737b22ef28d571
Maria13579 <patymsr3@gmail.com>
2021-09-27 19:16:39 -0600

    Z

--- templates_c/1d743650393ce5fbfe791b1c9a11dffbdb9812a8_0.file.Registro.tpl.php
+++ templates_c/1d743650393ce5fbfe791b1c9a11dffbdb9812a8_0.file.Registro.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-08-29 04:06:54
+/* Smarty version 3.1.39, created on 2021-09-28 03:15:17
   from 'C:\adobeTemp\htdocs\AtencionCliente\templates\Clientes\Registro.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_612aebbe8e63a2_61272269',
+  'unifunc' => 'content_61526ca57ea596_60351011',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '1d743650393ce5fbfe791b1c9a11dffbdb9812a8' => 
     array (
       0 => 'C:\\adobeTemp\\htdocs\\AtencionCliente\\templates\\Clientes\\Registro.tpl',
-      1 => 1630202749,
+      1 => 1630205294,
       2 => 'file',
     ),
   ),
@@ -23,10 +23,11 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/Cabeceras/Footer.tpl' => 1,
   ),
 ),false)) {
-function content_612aebbe8e63a2_61272269 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61526ca57ea596_60351011 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:templates/Cabeceras/Header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:Navs/Encabezado.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>  
+
 <h1 class="center-align"> Registro de Usuarios</h1>
 <div class="container">
 <div class="center-align">
