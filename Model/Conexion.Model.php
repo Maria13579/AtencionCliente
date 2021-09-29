227780b946b197f6d8cbde9ff7108a9fac7f7967
Maria13579 <patymsr3@gmail.com>
2021-09-29 15:01:57 -0600

    a

--- Model/Conexion.Model.php
+++ Model/Conexion.Model.php
@@ -7,11 +7,7 @@
             $pass="";
             $user="root";
             $DB="proyecto";
-            /*$servidor="mysql.webcindario.com";
-            $pass="Corona2468@";
-            $user="atencionmk";
-            $DB="atencionmk";*/
-
+        
             parent::__construct($servidor,$user,$pass,$DB);
             $this->query("SET NAMES 'utf8';");
             $this->connect_errno ? die('Error en la conexion') : $error='Conectado a '.$DB;
